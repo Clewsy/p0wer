775b6708ecf8f696cbae8b3ceec1c8d8e497620f
Clewsy <jadon.clews@gmail.com>
2017-06-07 22:14:14 +1000

    Improved argument parsing efficiency.

--- p0wer.c
+++ p0wer.c
@@ -14,12 +14,15 @@
 #define CHANNEL_D_ON	28
 #define CHANNEL_D_OFF	29 
 
+//assign a value for determining desired channel control when parsing arguments.
 #define TURN_OFF	0
 #define TURN_ON		1
 
+//define the name of the executable to be used when showing usage.
+#define FILENAME	"p0wer"
+
 //easier to remember definitions from the executable arguments.
 //Note, argv is an array of strings.
-#define ARG_FILENAME	argv[0]
 #define ARG_CHANNEL	argv[1]
 #define ARG_ON_OR_OFF	argv[2]
 
@@ -28,9 +31,9 @@
 
 //This function is called if there is an error with the executable usage.
 //It will print to screen the correct usage then exit.
-void exit_usage(char* filename)
+void exit_usage(void)
 {
-	printf("Usage: %s <channel(a,b,c,d)> <on/off>\n", filename);
+	printf("Usage: %s <channel(a,b,c,d)> <on/off>\n", FILENAME);
 	exit(EXIT_FAILURE);
 }
 
@@ -49,108 +52,92 @@ void control_outlet(int pin)
 	digitalWrite(pin, LOW);		//bring the pin back town low
 }
 
+int get_pin(char channel, char* on_or_off)
+{
+	//initialise integer "set" which will become either TURN_ON (1) or TURN_OFF (0)	
+	int set;
+
+        //confirm the first character of the on_or_off string is either 'o' or 'O' for on/off
+        if(on_or_off[0] != 'o' && on_or_off[0] != 'O'){ exit_usage(); }
+
+        //determine the intended contol by the second character of the string.
+        //i.e. 'n' for on or 'f' for off
+        switch(on_or_off[1])
+        {
+                case 'n' :
+                case 'N' :      //desired control is to set the channel on
+				//since the second character is 'n', confirm there is no third
+				//character.  else show syntax and exit.
+			if(on_or_off[2]){ exit_usage(); }
+                        set = TURN_ON;
+                        break;
+                case 'f' :
+                case 'F' :      //desired control is to set the channel off
+                		//since the second char is 'f', confirm the third char is also 'f'.
+                		//else show syntax and exit
+                        if(on_or_off[2] != 'f' && on_or_off[2] != 'F'){ exit_usage(); }
+                        set = TURN_OFF;
+                        break;
+                default :       //bad control argument entered.  show syntax and exit
+                        exit_usage();
+        }
+
+	//at this point we know the "channel" variable is a single character
+	//we are expecting it to be A, a, B, b, C, c, D or d.  This block
+	//will return the value of the pin to be pulsed (according to
+	//WiringPi numbering) to give the desired control. 
+        switch(channel)
+        {
+		case 'A' :
+                case 'a' :
+                        if (set)        { return(CHANNEL_A_ON); }
+                        else            { return(CHANNEL_A_OFF); }
+                        break;
+		case 'B' :
+                case 'b' :
+                        if (set)        { return(CHANNEL_B_ON); }
+                        else            { return(CHANNEL_B_OFF); }
+                        break;
+		case 'C' :
+                case 'c' :
+                        if (set)        { return(CHANNEL_C_ON); }
+                        else            { return(CHANNEL_C_OFF); }
+                        break;
+		case 'D' :
+                case 'd' :
+                        if (set)        { return(CHANNEL_D_ON); }
+                        else            { return(CHANNEL_D_OFF); }
+                        break;
+                default :               //unexpected char entered
+                        exit_usage();
+        }
+	
+	//Should never reach this point.
+	printf("Unknown error.  Quitting.\n");
+	exit(EXIT_FAILURE);
+}
 
 //main program loop.  arguments (argv[]) and number of arguments (argc)
 int main(int argc, char* argv[])
 {
+	//do a basic syntax check.  Show syntax and exit if something unexpected.
+
 	//first ensure the execution included the expected number of arguments
-	//(three including the executable).  else show syntax and exit
-	if (argc != 3)  { exit_usage(ARG_FILENAME); }
+	//(three including the executable).  else show syntax and exit.
+	if (argc != 3)  { exit_usage(); }
 
-	//need to determine from the arguments which channel is intended to
-	//be controlled and if it is to be set on or off
-	char channel;
-	int set;
+	//next confirm the second argument representing the desired
+	//channel is a single character.  else show syntax and exit.
+	if(strlen(ARG_CHANNEL) != 1){ exit_usage(); }
 
-	//following block determines the channel intended to be switched.
-	//only a single character will be accepted and then
-	//only A, a, B, b, C, c, D or d.
-	//first confirm the execution argument for the desired channel is
-	//a single character.  else show syntax and exit.
-	if(strlen(ARG_CHANNEL) != 1){ exit_usage(ARG_FILENAME); }
-	//determine desired channel
-	switch(ARG_CHANNEL[0])
-	{
-		case 'A' :
-		case 'a' :	//desired channel is 'a'
-			channel = 'a';
-			break;
-		case 'B' :
-		case 'b' :	//desired channel is 'b'
-			channel = 'b';
-			break;
-		case 'C' :
-		case 'c' :	//desired channel is 'c'
-			channel = 'c';
-			break;
-		case 'D' :
-		case 'd' :	//desired channel is 'd'
-			channel = 'd';
-			break;
-		default	:	//bad channel argument entered.  show syntax and exit.
-			exit_usage(ARG_FILENAME);
-	}
+	//then confirm the third argument is a string with no more than
+	//three characters.  else show syntax and exit
+	if(strlen(ARG_ON_OR_OFF) > 3){ exit_usage(); }
 
-	//following block determines if the selected channel should be turned
-	//on or off.  anything will be rejected if it is not on, oN, On, ON, off,
-	//ofF, oFf, oFF, Off, OfF, OFf or OFF
-	//first confirm the argument string has no more than three characters.
-	//else show syntax and exit
-	if(strlen(ARG_ON_OR_OFF) > 3){ exit_usage(ARG_FILENAME); }
-	//confirm the first character of the argument string is either 'o' or 'O' for on/off
-	if(ARG_ON_OR_OFF[0] != 'o' && ARG_ON_OR_OFF[0] != 'O'){ exit_usage(ARG_FILENAME); }
-
-	//determine the intended contol by the second character of the string.
-	//i.e. 'n' for on or 'f' for off
-	switch(ARG_ON_OR_OFF[1])
-	{
-		case 'n' :
-		case 'N' :	//desired control is to set the channel on
-			set = TURN_ON;
-			break;
-		case 'f' :
-		case 'F' :	//desired control is to set the channel off
-			//since the second char is 'f', confirm the third char is also 'f'.
-			//else show syntax and exit
-			if(ARG_ON_OR_OFF[2] != 'f' && ARG_ON_OR_OFF[2] != 'F'){ exit_usage(ARG_FILENAME); }
-			set = TURN_OFF;
-			break;
-		default :	//bad control argument entered.  show syntax and exit
-			exit_usage(ARG_FILENAME);
-	}
-
-	//at this pont we have verified the input arguments and know which
-	//channel to be operated (channel='a', 'b', 'c' or 'd') and if it
-	// should be set on (set=1) or off (set=0)
-	//next, use this information to determine which pin on the raspberry pi to pulse
-	//first initialise an integer representing the number of the pin
-	//corresponding to the desired control.  refer wiringPi
-	int pin_to_pulse;
-	switch(channel)
-	{
-		case 'a' :
-			if (set)	{ pin_to_pulse = CHANNEL_A_ON; }
-			else 		{ pin_to_pulse = CHANNEL_A_OFF; }	
-			break;
-		case 'b' :
-			if (set)        { pin_to_pulse = CHANNEL_B_ON; }
-			else            { pin_to_pulse = CHANNEL_B_OFF; }
-			break;
-		case 'c' :
-			if (set)        { pin_to_pulse = CHANNEL_C_ON; }
-			else            { pin_to_pulse = CHANNEL_C_OFF; }
-			break;
-		case 'd' :
-			if (set)        { pin_to_pulse = CHANNEL_D_ON; }
-			else            { pin_to_pulse = CHANNEL_D_OFF; }
-			break;
-		default :		//should never reach this
-			printf("Unknown error.  Quitting\n");
-			exit(EXIT_FAILURE);
-	}
-
-	//we now know which pin to pulse to give the desired control
-	control_outlet(pin_to_pulse);
+	//finally call the "get_pin" function to determine the desired pin
+	//to be pulsed, then pass it to the "control outlet" function to give
+	//the desired control
+	control_outlet(get_pin(ARG_CHANNEL[0], ARG_ON_OR_OFF));
 	return (EXIT_SUCCESS);
 }
 

cce3c74be6be3e27facc5d45befe5218f2801529
clewsy <jadon@clews.tech>
2025-11-01 15:37:16 +1100

    Second lamp on the network.

--- html/index.php
+++ html/index.php
@@ -1,27 +1,40 @@
 <?php
 
-// currently only one lamp on the network.
+    define("NUM_LAMPS", 2);
 
     // handle the toggle-all power button
-    if(isset($_GET['toggle_button_x'])){
+    if(isset($_GET['toggle_button_x'])) {
 
-        $control_url = "http://lamp2.lan/rpc/Switch.Toggle";
-        $control_id = "0";
+        for ($lamp = 0; $lamp < NUM_LAMPS; $lamp++) {
 
-        $curl_session_control = curl_init($control_url . "?id=" . $control_id);
+            $control_url = "http://lamp" . $lamp . ".lan/rpc/Switch.Toggle?id=0";
+
+            $curl_session_control = curl_init($control_url);
+            curl_setopt($curl_session_control, CURLOPT_RETURNTRANSFER, true);
+
+            curl_exec($curl_session_control);
+            curl_close($curl_session_control);
+        }
+    }
+
+    // handle the couch lamp button (lamp0)
+    if(isset($_GET['couch_button_x'])) {
+
+        $control_url = "http://lamp0.lan/rpc/Switch.Toggle?id=0";
+
+        $curl_session_control = curl_init($control_url);
         curl_setopt($curl_session_control, CURLOPT_RETURNTRANSFER, true);
 
         curl_exec($curl_session_control);
         curl_close($curl_session_control);
     }
 
-    // handle the coffee lamp button
-    if(isset($_GET['coffee_button_x'])){
+    // handle the coffee lamp button (lamp1)
+    if(isset($_GET['coffee_button_x'])) {
 
-        $control_url = "http://lamp2.lan/rpc/Switch.Toggle";
-        $control_id = "0";
+        $control_url = "http://lamp1.lan/rpc/Switch.Toggle?id=0";
 
-        $curl_session_control = curl_init($control_url . "?id=" . $control_id);
+        $curl_session_control = curl_init($control_url);
         curl_setopt($curl_session_control, CURLOPT_RETURNTRANSFER, true);
 
         curl_exec($curl_session_control);
@@ -30,19 +43,22 @@
 
 
     // get current status of lamps
-    $status_url = "http://lamp2.lan/rpc/Switch.GetStatus";
-    $status_id = "0";
+    for ($lamp = 0; $lamp < NUM_LAMPS; $lamp++) {
+        
+        $status_url = "http://lamp" . $lamp . ".lan/rpc/Switch.GetStatus?id=0";
 
-    $curl_session_status = curl_init($status_url . "?id=" . $status_id);
-    curl_setopt($curl_session_status, CURLOPT_RETURNTRANSFER, true);
-
-    $status_json = curl_exec($curl_session_status);
-    curl_close($curl_session_status);
-
-    $status = json_decode($status_json, true); //make associative array from json
-
-    if ($status['output'])  $lamp_status_colour = "rgb(150,150,150)";
-    else                    $lamp_status_colour = "rgb(50,50,50)";
+        $curl_session_status = curl_init($status_url);
+        curl_setopt($curl_session_status, CURLOPT_RETURNTRANSFER, true);
+        
+        $status_json = curl_exec($curl_session_status);
+        curl_close($curl_session_status);
+        
+        $status = json_decode($status_json, true); //make associative array from json
+        
+        if ($status['output'])  $lamp_status_colour[$lamp] = "rgb(150,150,150)";
+        else                    $lamp_status_colour[$lamp] = "rgb(50,50,50)";
+        
+    }
 
 ?>
 
@@ -60,15 +76,25 @@
     <body>
         <h1><a href="/index.php">p0wer</a></h1>
 
+        <!-- power button - toggles both lamps -->
         <form method="get" action="index.php">
             <input type="image" class="button" src="images/power.png" name="toggle_button">
         </form>
 
+        <!-- couch button - toggles lamp0 -->
         <form method="get" action="index.php">
-            <div class="box" style="background-color: <?=$lamp_status_colour ?>">
+            <div class="box" style="background-color: <?=$lamp_status_colour[0] ?>">
+                <input type="image" class="button" src="images/couch.png" name="couch_button">
+            </div>
+        </form>
+
+        <!-- coffee button - toggles lamp1 -->
+        <form method="get" action="index.php">
+            <div class="box" style="background-color: <?=$lamp_status_colour[1] ?>">
                 <input type="image" class="button" src="images/coffee.png" name="coffee_button">
             </div>
         </form>
+
     </body>
 
 </html>
